abeb3eac0c9e1c80b0955f7d7857d35b0a69dcf1
KritapasSuwannawin <kritapas.ms@gmail.com>
2023-02-24 00:49:10 -0500

    bugfix: cookie not sent in production

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -30,7 +30,8 @@ const createSendToken = (userId, req, res) => {
   const cookieOptions = {
     expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
     httpOnly: true,
-    secure: req.secure,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   };
 
   res.cookie('jwt', token, cookieOptions);
@@ -149,6 +150,8 @@ exports.logoutAccount = (req, res) => {
   res.cookie('jwt', '', {
     expires: new Date(Date.now() + 10 * 1000),
     httpOnly: true,
+    secure: process.env.NODE_ENV === 'production',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
   });
   res.json({ status: 'success' });
 };
